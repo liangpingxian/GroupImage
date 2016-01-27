d5a7de71ada3d238b37e3b3ecf3227f32d4fb3d6
梁平贤 <liangpingxian@gmail.com>
2016-01-27 15:10:00 +0800

    头像生成
    头像生成

--- GroupImage/ViewController.m
+++ GroupImage/ViewController.m
@@ -20,72 +20,17 @@
     [super viewDidLoad];
     
     CGFloat y = 50;
+    CGFloat x = 0;
     
-    GroupView *imageView11 = [[GroupView alloc] initWithFrame:CGRectMake(10, y, 100, 100)];
-    [imageView11 caculateWithN:1];
-    [self.view addSubview:imageView11];
-    
-    GroupView *imageView12 = [[GroupView alloc] initWithFrame:CGRectMake(130, y, 100, 100)];
-    
-    [imageView12 caculateWithN:2];
-    [self.view addSubview:imageView12];
-    
-    
-    GroupView *imageView13 = [[GroupView alloc] initWithFrame:CGRectMake(250, y, 100, 100)];
-    [imageView13 caculateWithN:3];
-    [self.view addSubview:imageView13];
-    
-    y += 110;
-    
-    GroupView *imageView14 = [[GroupView alloc] initWithFrame:CGRectMake(10, y, 100, 100)];
-    
-    [imageView14 caculateWithN:4];
-    [self.view addSubview:imageView14];
-    GroupView *imageView16 = [[GroupView alloc] initWithFrame:CGRectMake(130, y, 100, 100)];
-    
-    [imageView16 caculateWithN:5];
-    [self.view addSubview:imageView16];
-    
-    GroupView *imageView17 = [[GroupView alloc] initWithFrame:CGRectMake(250, y, 100, 100)];
-    
-    
-    y += 110;
-    
-    [imageView17 caculateWithN:6];
-    [self.view addSubview:imageView17];
-    
-    GroupView *imageView123 = [[GroupView alloc] initWithFrame:CGRectMake(10, y, 100, 100)];
-    
-    [imageView123 caculateWithN:7];
-    [self.view addSubview:imageView123];
-    
-    GroupView *imageView18 = [[GroupView alloc] initWithFrame:CGRectMake(130, y, 100, 100)];
-    
-    [imageView18 caculateWithN:8];
-    [self.view addSubview:imageView18];
-    
-    GroupView *imageView111 = [[GroupView alloc] initWithFrame:CGRectMake(250, y, 100, 100)];
-    
-    [imageView111 caculateWithN:9];
-    [self.view addSubview:imageView111];
-    
-    y += 110;
-    
-    
-    GroupView *imageView19 = [[GroupView alloc] initWithFrame:CGRectMake(10, y, 100, 100)];
-    
-    [imageView19 caculateWithN:10];
-    [self.view addSubview:imageView19];
-    
-    GroupView *imageView110 = [[GroupView alloc] initWithFrame:CGRectMake(130, y, 100, 100)];
-    
-    [imageView110 caculateWithN:11];
-    [self.view addSubview:imageView110];
-    
-    GroupView *imageView1111 = [[GroupView alloc] initWithFrame:CGRectMake(250, y, 100, 100)];
-    
-    [imageView1111 caculateWithN:12];
-    [self.view addSubview:imageView1111];
+    for (int i = 0; i<= 14; i++) {
+        x = (i % 3) * 110 + 10;
+        y = (i / 3) * 110 + 50;
+        
+        GroupView *imageView = [[GroupView alloc] initWithFrame:CGRectMake(x, y, 100, 100)];
+        [imageView caculateWithN:i + 1];
+        [self.view addSubview:imageView];
+        
+    }
     
     // Do any additional setup after loading the view, typically from a nib.
 }
